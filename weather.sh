af337bf07595e84c1aa6cbcb001ebc443ba7e0fa
Jeannie Klein <kleinje@i21b-s1.ufhpc>
2016-09-16 11:13:22 -0400

    test

--- weather.sh
+++ weather.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+#test comment
 curl "https://www.wunderground.com/history/airport/GNV/2016/09/06/DailyHistory.heml?&format=1" > gnv.txt
 
 maxTemp=`awk -F',' '{print $2}' gnv.txt | sort -n | tail -n1`
